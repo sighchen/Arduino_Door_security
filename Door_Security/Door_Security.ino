9eb9ba867cef6f53ad69274b816b2a4255b0f53e
Sigh Chen <simonchensigh@gmail.com>
2013-04-12 00:49:21 +0800

    Ping Sensor Added.

--- Door_Security/Door_Security.ino
+++ Door_Security/Door_Security.ino
@@ -1,7 +1,6 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <pitches.h>
-//#define melody 3
 LiquidCrystal lcd(31,33,35,37,39,41);
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -11,63 +10,37 @@ char hexaKeys[ROWS][COLS] = {
   {'8','9','A','B'},
   {'C','D','E','F'}
 };
-//char customeKey;
-//String buff;
 byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {13, 12, 11, 10}; //connect to the column pinouts of the keypad
 Keypad cusomKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 char PIN[6]={'1','2','A','D','5','6'}; // our secret (!) number
-char attempt[6]={
-  0,0,0,0,0,0}; // used for comparison
+char attempt[6]={0,0,0,0,0,0}; // used for comparison
 int z=0;
+int v=1;
+unsigned int EchoPin = 4;           // connect Pin 2(Arduino digital io) to Echo/RX at US-100
+unsigned int TrigPin = 5;           // connect Pin 3(Arduino digital io) to Trig/TX at US-100
+unsigned long Time_Echo_us = 0;
+unsigned long Len_mm  = 0;
 void setup(){
+  pinMode(EchoPin, INPUT);                    //Set EchoPin as input, to receive measure result from US-100
+  pinMode(TrigPin, OUTPUT);                   //Set TrigPin as output, used to send high pusle to trig measurement (>10us)
   lcd.begin(16,2);
   lcd.noAutoscroll();
   Serial.begin(9600);
   lcd.setCursor(0,0);
-  lcd.print("  Enter PIN...");
+ 
   Serial.print("Set the password ");
   cusomKeypad.setDebounceTime(20);
 
 }
   
-void loop(){
-  //char customKey = cusomKeypad.getKey();
- //String buffer=customKey;
-  //initLCDKeys();
- // char buff =customKey;
- // String KeKe = buff;
-  //if (customKey != NO_KEY){
- // lcd.setCursor(0,1);
- // lcd.print(buff);
- // Serial.print(customKey);
- readKeypad();
- // }
-}
-void checkPIN()
-{
-  int correct=0;
-  for (int q=0; q<6; q++)
-  {
-    if (attempt[q]==PIN[q])
-    {
-      correct++;
-    }
-  }
-  if (correct==6)
-  {
-    correctPIN();
-  } else
-  {
-    incorrectPIN();
-  }
-  for (int zz=0; zz<6; zz++) // wipe attempt
-  {
-    attempt[zz]=0;
-  }
-}
+void loop(){ 
+  ultrasonic();
+  readKeypad();
 
+}
 void readKeypad(){
+  
   char key = cusomKeypad.getKey();
   if (key != NO_KEY)
   {
@@ -84,6 +57,46 @@ void readKeypad(){
     default:
       attempt[z]=key;
       z++;    }
+  }
+}
+void ultrasonic(){
+  while(v==1){
+        digitalWrite(TrigPin, HIGH);              //begin to send a high pulse, then US-100 begin to measure the distance
+    delayMicroseconds(50);                    //set this high pulse width as 50us (>10us)
+    digitalWrite(TrigPin, LOW);               //end this high pulse
+    
+    Time_Echo_us = pulseIn(EchoPin, HIGH);               //calculate the pulse width at EchoPin, 
+    if((Time_Echo_us < 60000) && (Time_Echo_us > 1))     //a valid pulse width should be between (1, 60000).
+    {
+      Len_mm = (Time_Echo_us*34/100)/2;      //calculate the distance by pulse width, Len_mm = (Time_Echo_us * 0.34mm/us) / 2 (mm)
+      Serial.print("Present Distance is: ");  //output result to Serial monitor
+      Serial.print(Len_mm, DEC);            //output result to Serial monitor
+      Serial.println("mm");                 //output result to Serial monitor
+      if(Len_mm <= 100){
+        v--;
+        lcd.print("  Enter PIN...");
+      }
+    }
+    delay(1000);                            //take a measurement every second (1000ms)
+  }
+}
+  
+void checkPIN(){
+  int correct=0;
+  for (int q=0; q<6; q++)
+ {
+   if(attempt[q]==PIN[q]) {
+      correct++;
+    }
+ } 
+   if(correct==6){
+    correctPIN();
+  } 
+  else {
+    incorrectPIN();
+  }
+  for(int zz=0; zz<6; zz++){ // wipe attempt
+    attempt[zz]=0;
   }
 }
 void correctPIN() // do this if correct PIN entered
